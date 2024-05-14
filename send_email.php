b29eb2d5b973d813dae5d9898546bd8eca0b6008
MuhammadAfridi69 <168188117+MuhammadAfridi69@users.noreply.github.com>
2024-05-14 22:39:07 +0500

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "saadafriditv@gmail.com"; // Change to your desired email address
+    $subject = "Message from Website";
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+    
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    
+    if (mail($to, $subject, $message, $headers)) {
+        echo "<p>Email sent successfully. Thank you for contacting us!</p>";
+    } else {
+        echo "<p>Failed to send email. Please try again later.</p>";
+    }
+}
+?>
